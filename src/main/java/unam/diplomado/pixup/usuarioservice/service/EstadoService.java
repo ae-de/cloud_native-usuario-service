54285a6172379415810a0040bce68af49121c8df
ae-de <67604088+ae-de@users.noreply.github.com>
2023-09-29 19:47:30 -0600

    Delete src directory

--- src/main/java/unam/diplomado/pixup/usuarioservice/service/EstadoService.java
+++
@@ -1,9 +0,0 @@
-package unam.diplomado.pixup.usuarioservice.service;
-
-import unam.diplomado.pixup.usuarioservice.domain.Estado;
-
-public interface EstadoService {
-	
-	Estado actualizarEstado(String id, Estado estado);
-}
-
